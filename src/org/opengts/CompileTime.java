bc59dfb9021d0d07df4dbced84170a2ec0090156
Rodrigo <Rodrigo@Rodrigo>
2017-05-20 16:31:10 -0300

    tiempoCompilación

--- src/org/opengts/CompileTime.java
+++ src/org/opengts/CompileTime.java
@@ -1,12 +1,12 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2017/05/20 13:42:20 ART
+    // 2017/05/20 16:15:23 ART
     public static final String SERVICE_ACCOUNT_ID   = "opengts";
     public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
     public static final String SERVICE_ACCOUNT_HASH = "8E8AD5BD55CCBA73A679AC720DC08B8A";
     public static final String SERVICE_ACCOUNT_KEY  = "";
-    public static final long   COMPILE_TIMESTAMP    = 1495298540L;
-    public static final String COMPILE_DATETIME     = "2017/05/20 13:42:20 ART";
-    public static final long   COMPILE_SEED         = 0x12E37B1C932BA041L;
+    public static final long   COMPILE_TIMESTAMP    = 1495307723L;
+    public static final String COMPILE_DATETIME     = "2017/05/20 16:15:23 ART";
+    public static final long   COMPILE_SEED         = 0x6756F0040A75CE66L;
 }
